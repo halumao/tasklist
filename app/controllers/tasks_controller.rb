f13f2866bbd71e1f8a42f40f47ae735c86571e97
halu <s17a1013@bene.fit.ac.jp>
2020-02-18 09:25:03 +0000

    tasklist

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,81 @@
+class TasksController < ApplicationController
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:edit, :destroy, :show, :update]
+
+
+  def index
+    if logged_in?
+      @tasks = current_user.tasks
+    else
+      redirect_to login_path
+    end
+  end
+  
+  def show 
+
+  end
+
+
+  def new
+       @task = current_user.tasks.build
+  end
+
+  def create
+       @task = current_user.tasks.build(task_params)
+
+    if @task.save
+      flash[:success] = 'task が正常に投稿されました'
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'Task が投稿されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+
+  
+ 
+  end
+
+  def update
+ 
+      
+    
+
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+
+    @task.destroy
+
+    flash[:success] = 'task は正常に削除されました'
+    redirect_back(fallback_location: root_path)
+ 
+  end
+  
+    private
+
+      # Strong Parameter
+      def task_params
+        params.require(:task).permit(:content ,:status)
+      end
+    
+    
+      
+    def correct_user
+            @task = current_user.tasks.find_by(id: params[:id])
+          unless @task
+           redirect_to root_url
+          end
+    end
+    
+end
+
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:create]
+  root to: 'tasks#index'
+  resources :tasks, only:[:new, :create, :edit, :destroy, :update, :show]
+
+  
+end
